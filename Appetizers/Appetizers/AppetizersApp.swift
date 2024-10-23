87bf91381679a11d2a871b5e3690e1466aa4ef9a
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-23 20:25:05 +0200

    Setting up

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Erik Sebastian de Erice Jerez on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
