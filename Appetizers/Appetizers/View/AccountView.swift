5e54c228b1a1c6d7c10de4b10cb4079c9480b4d2
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-29 16:21:01 +0100

    Creating the Profile view

--- Appetizers/Appetizers/View/AccountView.swift
+++ Appetizers/Appetizers/View/AccountView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var email: String = ""
+    @State private var birthDate: Date = Date()
+    @State private var hasExtraNapkins: Bool = false
+    @State private var hasFrequentRefill: Bool = false
+    
     var body: some View {
         NavigationStack {
             VStack {
-                
+                Form {
+                    Section("Personal information") {
+                        TextField("First Name", text: $firstName)
+                        
+                        TextField("Last Name", text: $lastName)
+                        
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.never)
+                        
+                        DatePicker("Birth Date", selection: $birthDate, displayedComponents: .date)
+                        
+                        Button("Save Changes") {
+                            
+                        }
+                    }
+                    
+                    Section("Request") {
+                        Toggle("Extra Napkings", isOn: $hasExtraNapkins)
+                        Toggle("Frequent Refill", isOn: $hasFrequentRefill)
+                    }
+                    .tint(.brandPrimary)
+                }
             }
             .navigationTitle("Account")
         }
