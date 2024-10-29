840b3581adb67f1a413c6135f22f1dae8996ac55
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-30 00:36:04 +0100

    Native regex

--- Appetizers/Appetizers/Utils/Extension/String.swift
+++ Appetizers/Appetizers/Utils/Extension/String.swift
@@ -6,9 +6,37 @@
 //
 
 import Foundation
+import RegexBuilder
 
 extension String {
     var isValidEmail: Bool {
-        NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}").evaluate(with: self)
+        let emailRegex = Regex {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("._%+-"),
+                    ("A"..."Z"),
+                    ("0"..."9"),
+                    ("a"..."z")
+                )
+            }
+            "@"
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("-"),
+                    ("A"..."Z"),
+                    ("a"..."z"),
+                    ("0"..."9")
+                )
+            }
+            "."
+            Repeat(2...64) {
+                CharacterClass(
+                    ("A"..."Z"),
+                    ("a"..."z")
+                )
+            }
+        }
+        
+        return self.wholeMatch(of: emailRegex) != nil
     }
 }
