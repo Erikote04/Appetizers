78480223568a08487ef3046801196046b0886b19
Erik Sebastián de Erice Jerez <erikote2004@icloud.com>
2024-10-29 14:06:36 +0100

    Creating the detail view

---
+++ Appetizers/Appetizers/View/AppetizerDetailView.swift
@@ -0,0 +1,95 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizers
+//
+//  Created by Erik Sebastian de Erice Jerez on 29/10/24.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let appetizer: AppetizerItem
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            VStack {
+                Image("asian-flank-steak")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity)
+                
+                Text(appetizer.name)
+                    .font(.title.bold())
+                    .padding()
+                
+                Text(appetizer.description)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                
+                HStack(spacing: 32) {
+                    VStack(spacing: 8) {
+                        Text("Calories")
+                            .font(.caption.bold())
+                        
+                        Text("\(appetizer.calories)")
+                            .fontWeight(.semibold).italic()
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    VStack(spacing: 8) {
+                        Text("Carbs")
+                            .font(.caption.bold())
+                        
+                        Text("\(appetizer.carbs) g")
+                            .fontWeight(.semibold).italic()
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    VStack(spacing: 8) {
+                        Text("Protein")
+                            .font(.caption.bold())
+                        
+                        Text("\(appetizer.protein) g")
+                            .fontWeight(.semibold).italic()
+                            .foregroundStyle(.secondary)
+                    }
+                }
+                .padding(.top, 24)
+                
+                Spacer()
+            }
+            .overlay(
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .padding()
+                        .foregroundStyle(.black)
+                        .background(.thinMaterial)
+                        .clipShape(.circle)
+                }.padding(),
+                alignment: .topTrailing
+            )
+            
+            Button {
+                // Add to order
+            } label: {
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to order")
+                    .font(.title3.bold())
+                    .padding()
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.brandPrimary)
+                    .clipShape(.rect(cornerRadius: 12))
+                
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+}
